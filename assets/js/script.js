ed658905f6cd708b945f91eca2a0e513f4c2efc4
Emma Olson <emma.olson794@gmail.com>
2023-05-30 21:06:14 -0500

    basic list of bathrooms is printing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 const locationForm = document.querySelector('#location-form');
+const bathroomUl = document.querySelector('#bathroom-data');
 
+// api handler function
 function getBathroom(url) {
     fetch(url)
     .then(function (res) {
@@ -7,21 +9,43 @@ function getBathroom(url) {
     })
     .then(function (data) {
         console.log(data);
+
+        // map through the retrieved data and print to the page
+        data.map(function(data) {
+            console.log(data);
+            const newBathroomElHeader = document.createElement("li");
+            newBathroomElHeader.textContent = data.name;
+            bathroomUl.append(newBathroomElHeader);
+
+            // check if the bathroom is marked as accessible
+            if (data.accessible) {
+                newBathroomElHeader.append(' ♿ ');
+            };
+
+            // check if the bathroom is marked as unisex
+            if (data.unisex) {
+                newBathroomElHeader.append(' Unisex ');
+            };
+
+            
+        })
     })
 };
 
 function getData(event) {
 
     event.preventDefault();
+
     let location = document.querySelector('#location-input').value;
-    console.log(location);
     let latitude = "33.753746";
     let longitude = "-84.386330";
-    let bathroomApiUrl = `https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0&lat=${latitude}&lng=${longitude}`;
     
+    // check to make sure we have the proper data to make the api call
     if (latitude && longitude) {
+        let bathroomApiUrl = `https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0&lat=${latitude}&lng=${longitude}`;
         getBathroom(bathroomApiUrl);
     }
 }
 
+// event listener for form submission
 locationForm.addEventListener('submit', getData)
